cec0dc12d653bae4ad793120e112333faedd0abb
Alexander <ixav1@icloud.com>
2019-04-24 18:13:29 +0300

    reorganized struct
    reorganized struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,53 +1,39 @@
+/// LIBSVMParser is for parsing libsvm file format.
 use std::path::Path;
 use std::fs::File;
 use std::collections::HashSet;
 use std::io::{BufReader, BufRead};
 use std::str::FromStr;
 use std::fmt::Debug;
+use std::hash::Hash;
 
 #[derive(Debug)]
-pub struct LIBSVMParser<'libsvm> {
-  pub file: &'libsvm Path
+pub struct LIBSVMParser {
+  is_classif: bool
 }
 
-macro_rules! parse_file {
-  ($type: ty, $file: expr) => {{
-    let mut buf_reader = BufReader::new($file);
-    let mut line = String::new();
-    let mut data: Vec<Vec<$type>> = Vec::new();
-    while buf_reader.read_line(&mut line)
-      .expect("Error in reading file") > 0 {
-      line = line.trim().to_string();
-      let mut values = line.split_whitespace();
-      let mut sample: Vec<$type> = Vec::new();
-      let target = match values.next() {
-        Some(target) => {
-          match target.parse::<$type>() {
-            Ok(target) => target,
-            Err(_) => panic!("Error in parsing file.")
-          }
-        },
-        None => panic!("Error in parsing file.")
-      };
-      sample.push(target);
-      for value in values {
-        let feature: Vec<&str> = value.split(':').collect();
-        let feature = *(feature.last().unwrap());
-        match feature.parse::<$type>() {
-          Ok(f) => sample.push(f),
-          Err(_) => panic!("Error in parsing file.")
-        }
-      }
-      data.push(sample);
-      line.clear();
+impl LIBSVMParser {
+
+  pub fn new() -> Self {
+    Self {
+      is_classif: false
     }
-    data
-  }};
-}
+  }
 
-impl<'libsvm> LIBSVMParser<'libsvm> {
-  pub fn new(file: &'libsvm Path) -> Self {
-    if file.exists() {
+  /// If init with true, than read_file returns HashSet with unique classes.
+  pub fn is_classification(self, is_classification: bool) -> Self {
+    Self {
+      is_classif: is_classification,
+      ..self
+    }
+  }
+
+  /// Read and parse libsvm file line by line.
+  pub fn parse_file<T, U>(&self, file: &Path) ->
+    (Vec<Vec<T>>, Option<HashSet<U>>) where
+    T: FromStr + Debug,
+    U: FromStr + Debug + Hash + Eq {
+    let file = if file.exists() {
       let ext = match file.extension() {
         Some(ext) => ext,
         None => panic!("Wrong file format, expected .libsvm")
@@ -55,20 +41,59 @@ impl<'libsvm> LIBSVMParser<'libsvm> {
       if ext != "libsvm" {
         panic!("Wrong file format, expected .libsvm but found: {:?}", ext);
       }
-      Self {
-        file: file
-      }
+      file
     } else {
       panic!("File {:?} doesn't exist.", file);
-    }
-  }
-
-  /// Read and parse libsvm file line by line.
-  pub fn read_file<T: FromStr + Debug>(&self) -> Vec<Vec<T>> {
-    let file = match File::open(self.file) {
+    };
+    let file = match File::open(file) {
       Ok(file) => file,
       Err(err) => panic!("Error in opening file. {:?}", err)
     };
-    parse_file!(T, file)
+    let mut buf_reader = BufReader::new(file);
+    let mut line = String::new();
+    let mut data: Vec<Vec<T>> = Vec::new();
+    let mut classes: Option<HashSet<U>> = None;
+    if self.is_classif {
+      classes = Some(HashSet::new());
+    }
+    let mut line_num = 1;
+    while buf_reader.read_line(&mut line)
+      .expect("Error in reading file") > 0 {
+      line = line.trim().to_string();
+      let mut values = line.split_whitespace();
+      let mut sample: Vec<T> = Vec::new();
+      let target = values.next();
+      match target {
+        Some(target) => {
+          match target.parse::<T>() {
+            Ok(target) => sample.push(target),
+            Err(_) => panic!("Error in parsing target. Error in line: {:?}", line_num)
+          }
+          match classes {
+            Some(ref mut classes) => {
+              match target.parse::<U>() {
+                Ok(target) => { classes.insert(target); },
+                Err(_) => panic!("Error in parsing uniq classes. Error in line: {:?}", line_num)
+              }
+            },
+            None => ()
+          }
+        },
+        None => panic!("Error in parsing target. Error in line: {:?}", line_num)
+      }
+      for value in values {
+        let feature: Vec<&str> = value.split(':').collect();
+        let feature = *(feature.last().unwrap());
+        match feature.parse::<T>() {
+          Ok(f) => sample.push(f),
+          Err(_) => panic!("Error in parsing feature. Error in line: {:?}", line_num)
+        }
+      }
+      data.push(sample);
+      line.clear();
+      line_num += 1;
+    }
+    (data, classes)
   }
+
 }
--- tests/test.rs
+++ tests/test.rs
@@ -3,13 +3,9 @@ extern crate pretty_assertions;
 extern crate libsvm_parser;
 
 use std::path::Path;
+use std::collections::HashSet;
+use std::iter::FromIterator;
 use libsvm_parser::*;
-
-#[test]
-fn test_creating_exist() {
-  let file_exist = Path::new("./tests/data/exist_empty.libsvm");
-  let _parser = LIBSVMParser::new(file_exist);
-}
 
 #[test]
 fn test_read_file_by_line() {
@@ -24,12 +20,34 @@ fn test_read_file_by_line() {
                   vec![4.0, 4.4, 2.9, 1.4, 0.2],
                   vec![-6.0, 4.9, 3.1, 1.5, 0.1]];
   let file_exist = Path::new("./tests/data/test.libsvm");
-  let parser = LIBSVMParser::new(file_exist);
-  let result = parser.read_file::<f32>();
+  let parser = LIBSVMParser::new();
+  let (result, _) = parser.parse_file::<f32, i32>(file_exist);
 
   for (i, row) in data.iter().enumerate() {
     for (j, value) in row.iter().enumerate() {
       assert_eq!(*value, result[i][j]);
     }
   }
+}
+
+#[test]
+fn test_read_file_for_classif() {
+  let data = vec![vec![0.0, 5.1, 3.5, 1.4, 0.2],
+                  vec![1.0, 4.9, 3.0, 1.4, 0.2],
+                  vec![0.0, 4.7, 3.2, 1.3, 0.2],
+                  vec![2.0, 4.6, 3.1, 1.5, 0.2],
+                  vec![3.0, 5.0, 3.6, 1.4, 0.2]];
+  let classes: Vec<i32> = vec![0, 1, 0, 2, 3];
+  let classes_set: HashSet<i32> = HashSet::from_iter(classes.iter().cloned());
+  let file_exist = Path::new("./tests/data/test1.libsvm");
+  let parser = LIBSVMParser::new().is_classification(true);
+  let (result, res_classes) = parser.parse_file::<f32, i32>(file_exist);
+  let res_classes = res_classes.unwrap();
+  for (i, row) in data.iter().enumerate() {
+    for (j, value) in row.iter().enumerate() {
+      assert_eq!(*value, result[i][j]);
+    }
+  }
+  let diff: HashSet<_> = res_classes.difference(&classes_set).collect();
+  assert!(diff.is_empty());
 }
